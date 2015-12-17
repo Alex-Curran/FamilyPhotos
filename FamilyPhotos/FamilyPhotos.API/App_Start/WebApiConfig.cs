072aac3909b5b9d2ed744487563e7e05dcc027bb
Alex Curran <Alex-Curran@outlook.com>
2015-12-17 16:10:46 -0600

    Intial Commit

--- FamilyPhotos/FamilyPhotos.API/App_Start/WebApiConfig.cs
+++ FamilyPhotos/FamilyPhotos.API/App_Start/WebApiConfig.cs
@@ -1,24 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json.Serialization;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace FamilyPhotos.API
 {
     public static class WebApiConfig
     {
+        public static object SerializerSettings { get; private set; }
+
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new { id = RouteParameter.Optional });
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
+
     }
 }
---
+++ FamilyPhotos/FamilyPhotos.API/Controllers/PhotoController.cs
@@ -0,0 +1,36 @@
+﻿using FamilyPhotos.Logic;
+using FamilyPhotos.Models;
+using System.Web.Http;
+
+namespace FamilyPhotos.API.Controllers
+{
+    [Authorize]
+    public class PhotoController : ApiController
+    {
+        private readonly PhotoLogic logic = new PhotoLogic();
+
+        // GET: api/photo/id
+        // returns photo with specfied id
+        public IHttpActionResult GET(int id)
+        {
+            Photo photo = logic.GetById(id);
+            if(photo == null)
+            {
+                return BadRequest("Photo not found");
+            }
+            else
+            {
+                return Ok(photo);
+            }
+           
+        }
+
+        // POST: api/photo
+        // Saves Photo
+        public IHttpActionResult POST(PhotoViewModel photoViewModel)
+        {
+
+            return Ok();
+        }
+    }
+}
