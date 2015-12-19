fe5412bf6fd8d01521082f1c6227ca7217952668
Alex Curran <Alex-Curran@outlook.com>
2015-12-19 16:46:29 -0600

    Album Controller added

---
+++ FamilyPhotos/FamilyPhotos.API/Controllers/AlbumController.cs
@@ -0,0 +1,147 @@
+﻿using FamilyPhotos.Logic;
+using FamilyPhotos.Models;
+using FamilyPhotos.Models.ViewModels;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace FamilyPhotos.API.Controllers
+{
+    [Authorize]
+    public class AlbumController : ApiController
+    {
+        private readonly AlbumLogic logic = new AlbumLogic();
+
+        // GET: api/Album
+        // Returns all Albums
+        public IHttpActionResult Get()
+        {
+            Result<List<AlbumViewModel>> result = new Result<List<AlbumViewModel>>();
+
+            result = logic.GetAllAlbums();
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            else if(result.InternalError)
+            {
+                return new HttpActionResult(HttpStatusCode.InternalServerError, result.ErrorMessage);
+            }
+            else
+            {
+                return new HttpActionResult(HttpStatusCode.BadRequest, result.ErrorMessage);
+            }
+
+        }
+
+        // GET: api/Album/5
+        // Returns specific Album
+        public IHttpActionResult Get(int id)
+        {
+            Result<AlbumViewModel> result = new Result<AlbumViewModel>();
+            result = logic.GetAlbumById(id);
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            else if(result.InternalError)
+            {
+                return new HttpActionResult(HttpStatusCode.InternalServerError, result.ErrorMessage);
+            }
+            else
+            {
+                return new HttpActionResult(HttpStatusCode.BadRequest, result.ErrorMessage);
+            }
+        }
+
+        //GET: api/GetUsersAlbums/userName
+        // Returns all the albums for a user
+        public IHttpActionResult GetUsersAlbums(string userName)
+        {
+            Result<List<Album>> result = new Result<List<Album>>();
+            result = logic.GetAlbumsForUser(userName);
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            else if(result.InternalError)
+            {
+                return new HttpActionResult(HttpStatusCode.InternalServerError, result.ErrorMessage);
+            }
+            else
+            {
+                return new HttpActionResult(HttpStatusCode.BadRequest, result.ErrorMessage);
+            }
+
+        }
+
+        // POST: api/Album
+        // Creates an Album
+        public IHttpActionResult Post([FromBody]Album album)
+        {
+            Result result = new Result();
+            result = logic.Add(album);
+
+            if (result.Success)
+            {
+                return new HttpActionResult(HttpStatusCode.Created, "Album created");
+            }
+            else if(result.InternalError)
+            {
+                return new HttpActionResult(HttpStatusCode.InternalServerError, result.ErrorMessage);
+            }
+            else
+            {
+                return new HttpActionResult(HttpStatusCode.BadRequest, result.ErrorMessage);
+            }
+
+        }
+
+        // DELETE: api/Album
+        // Deletes an album
+        public IHttpActionResult Delete(Album album)
+        {
+            Result result = new Result();
+
+            result = logic.Delete(album);
+
+            if (result.Success)
+            {
+                return new HttpActionResult(HttpStatusCode.OK, "Successfully Deleted");
+            }
+            else if (result.InternalError)
+            {
+                return new HttpActionResult(HttpStatusCode.InternalServerError, result.ErrorMessage);
+            }else
+            {
+                return new HttpActionResult(HttpStatusCode.BadRequest, result.ErrorMessage);
+            }
+        }
+
+        //PUT: api/Album
+        //Updates the album 
+        public IHttpActionResult PUT(Album album)
+        {
+            Result result = new Result();
+            result = logic.Update(album);
+
+            if (result.Success)
+            {
+                return new HttpActionResult(HttpStatusCode.OK, "Album updated");
+
+            }
+            else if(result.InternalError)
+            {
+                return new HttpActionResult(HttpStatusCode.InternalServerError, result.ErrorMessage); 
+            }
+            else
+            {
+                return new HttpActionResult(HttpStatusCode.BadRequest, result.ErrorMessage);
+
+            }
+        }
+    }
+}
---
+++ FamilyPhotos/FamilyPhotos.API/Startup.cs
@@ -0,0 +1,26 @@
+﻿using FamilyPhotos.API;
+using FamilyPhotos.WebAPI;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace FamilyPhotos.WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            ConfigureAuthZero(app);
+
+            // TODO: Not recommended for production
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
